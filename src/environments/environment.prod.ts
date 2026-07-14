c08c6693d9a5f873dc0c15ec7ae35c691b75cb89
Ahmed-Selim076 <xxahmedwork@gmail.com>
2026-07-15 01:08:15 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl:         'https://launchly-api076.up.railway.app/api',
-  platformDomain: 'launchly-api076.up.railway.app',
+  platformDomain: 'launchly-frontend.vercel.app',
   googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
 } as const;
